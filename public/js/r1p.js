0a3662c8f5c4cdde68997b540e096976257a136c
Jasem Qabazard <jasemqabazard@yahoo.com>
2016-10-20 17:21:30 +0700

    update201610201650

--- public/js/r1p.js
+++ public/js/r1p.js
@@ -1,4 +1,19 @@
 ﻿'use strict';
 angular.module('r1p', ['validation.match', 'ui.bootstrap'])
+        .config(['$httpProvider', function ($httpProvider) {
+            //initialize get if not there
+            $httpProvider.defaults.cache = false;
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+            }
 
+            // Answer edited to include suggestions from comments
+            // because previous version of code introduced browser-related errors
+
+            //disable IE ajax request caching
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            // extra
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }])
 ;
